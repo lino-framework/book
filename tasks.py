d528447a90cdd0cd9461d1aa22f2ec974a599c8a
Luc Saffre <luc.saffre@gmail.com>
2016-07-15 19:59:53 +0300

    http://luc.lino-framework.org/blog/2016/0715.html

--- tasks.py
+++ tasks.py
@@ -1,15 +1,11 @@
 from atelier.tasks import ns
-ns.setup_from_tasks(globals(), 'lino_book')
-
-# tolerate_sphinx_warnings: true
-# blogger_project: blog
-ns.configure(dict(
+ns.setup_from_tasks(
+    globals(), 'lino_book',
+    # tolerate_sphinx_warnings=True,
     blogref_url="http://luc.lino-framework.org",
     revision_control_system='git',
     help_texts_source='docs',
-    help_texts_module='lino_xl.lib.xl'))
-
-ns.configure(dict(
+    help_texts_module='lino_xl.lib.xl',
     cleanable_files=[
         'docs/rss_entry_fragments/*',
         'docs/api/lino.*',
@@ -23,4 +19,4 @@ ns.configure(dict(
         'lino_book.projects.max.settings.demo',
         'lino_book.projects.i18n.settings',
         'lino_book.projects.min1.settings.demo',
-        'lino_book.projects.min2.settings.demo']))
+        'lino_book.projects.min2.settings.demo'])
