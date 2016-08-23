bbf3c78a55d105a60da59660b7a76eb399970009
Hamza Khchine <hamzakhchine@gmail.com>
2016-08-23 07:57:43 +0100

    Add Readthedocs' badge to setup_info

--- lino_book/setup_info.py
+++ lino_book/setup_info.py
@@ -72,6 +72,7 @@ else:
     SETUP_INFO['install_requires'].append('reportlab')
 
 SETUP_INFO.update(long_description="""\
+|docs|
 
 The Lino Book is a code repository used for educational and testing
 purposes.  It contains the big Sphinx documentation tree about the
@@ -85,6 +86,11 @@ have no documentation tree on their own, their documentation is here.
 
 It also contains a big test suite which runs doctest-based tests for
 all these packages.
+
+.. |docs| image:: https://readthedocs.org/projects/lino/badge/?version=latest
+    :alt: Documentation Status
+    :scale: 100%
+    :target: http://lino.readthedocs.io/en/latest/?badge=latest
 
 """)
 
