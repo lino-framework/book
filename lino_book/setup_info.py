74bc2e2fd8e0a549edd5c5a2ad79ab2aa290d6ee
Hamza Khchine <hamzakhchine@gmail.com>
2016-08-24 23:25:57 +0100

    Add new badges to book project

--- lino_book/setup_info.py
+++ lino_book/setup_info.py
@@ -72,7 +72,7 @@ else:
     SETUP_INFO['install_requires'].append('reportlab')
 
 SETUP_INFO.update(long_description="""\
-|book_docs|
+|book_docs| |coverage| |build| |pypi_v| |pypi_license|
 
 The Lino Book is a code repository used for educational and testing
 purposes.  It contains the big Sphinx documentation tree about the
@@ -91,6 +91,18 @@ all these packages.
     :alt: Documentation Status
     :scale: 100%
     :target: http://lino.readthedocs.io/en/latest/?badge=latest
+
+.. |coverage| image:: https://coveralls.io/repos/github/lino-framework/book/badge.svg?branch=master
+    :target: https://coveralls.io/github/lino-framework/book?branch=master
+
+.. |build| image:: https://travis-ci.org/lino-framework/book.svg?branch=master
+    :target: https://travis-ci.org/lino-framework/book?branch=master
+
+.. |pypi_v| image:: https://img.shields.io/pypi/v/lino.svg
+    :target: https://pypi.python.org/pypi/lino/
+
+.. |pypi_license| image:: https://img.shields.io/pypi/l/lino.svg
+    :target: https://pypi.python.org/pypi/lino/
 
 """)
 
