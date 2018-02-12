70c06faa7faf2180af91c3718596b39429066919
Luc Saffre <luc.saffre@gmail.com>
2018-02-12 22:00:06 +0200

    http://luc.lino-framework.org/blog/2018/0212.html

--- lino_book/projects/events/settings.py
+++ lino_book/projects/events/settings.py
@@ -1,38 +1,21 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2013-2014 Luc Saffre
+# Copyright 2013-2018 Luc Saffre
 # License: BSD (see file COPYING for details)
-
-"""
-
-"""
 
 
 from __future__ import unicode_literals
 
-#~ try:
-
 from lino.projects.std.settings import *
-
-#~ from django.utils.translation import ugettext_lazy as _
 
 
 class Site(Site):
 
     title = "Lino Events"
     verbose_name = "Lino Events"
-    #~ verbose_name = "Lino Cosi"
-    #~ description = _("a Lino application to make Belgian accounting simple.")
-    #~ version = "0.1"
-    #~ url = "http://www.lino-framework.org/autodoc/lino.projects.cosi"
-    #~ author = 'Luc Saffre'
-    #~ author_email = 'luc.saffre@gmail.com'
 
-    # demo_fixtures = 'std few_countries few_cities vor'.split()
     demo_fixtures = 'demo vor'.split()
 
     languages = 'de fr nl'
-    #~ languages = ['de','fr','nl']
-    #~ languages = 'de fr et en'.split()
 
     def get_installed_apps(self):
         yield super(Site, self).get_installed_apps()
@@ -43,8 +26,4 @@ class Site(Site):
 
 SITE = Site(globals())
 
-#~ except Exception as e:
-    #~ import traceback
-    #~ traceback.print_exc()
-    #~ sys.exit(1)
-  #~
+DEBUG = True
