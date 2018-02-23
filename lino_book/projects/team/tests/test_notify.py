94c38a037d51f118cce659cb1cc960fd0bc54e85
Luc Saffre <luc.saffre@gmail.com>
2018-02-23 10:01:50 +0200

    #2327 remove Comment.published, restore ChangeObservable

--- lino_book/projects/team/tests/test_notify.py
+++ lino_book/projects/team/tests/test_notify.py
@@ -190,7 +190,7 @@ Robin a comment? <a href="http://127.0.0.1:8000/api/tickets/Ticket/1" title="Sav
         # UnicodeException. We capture it in order to hide it from
         # test runner output.
         
-        self.assertEqual(logger.debug.call_count, 1)
+        self.assertEqual(logger.debug.call_count, 2)
         logger.debug.assert_called_with(
             'Send out %s summaries for %d users.',
             MailModes.often, 1)
