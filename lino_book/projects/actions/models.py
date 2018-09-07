fc89b6876b8fe7fc51df030868710e544728112b
khchine5 <hamzakhchine@gmail.com>
2018-09-07 07:20:44 +0100

    #2195 : Add python2/3 compatibility.

--- lino_book/projects/actions/models.py
+++ lino_book/projects/actions/models.py
@@ -1,35 +1,46 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from lino.api import dd, rt
 
+
 class A(dd.Action):
     label = _("a")
-    def run_from_ui(self,ar,**kw):
+
+    def run_from_ui(self, ar, **kw):
         obj = ar.selected_rows[0]
         return ar.success("Called a() on %s" % obj)
 
+
+@python_2_unicode_compatible
 class Moo(dd.Model):
     a = A()
+
+    def __str__(self):
+        return '%s object' % (self.__class__.__name__)
+
     @dd.action(_("m"))
-    def m(self,ar,**kw):
+    def m(self, ar, **kw):
         return ar.success("Called m() on %s" % self)
-        
+
+
 class Moos(dd.Table):
     model = Moo
-    
+
     b = A()
-    
+
     @dd.action(_("t"))
-    def t(obj,ar,**kw):
+    def t(obj, ar, **kw):
         return ar.success("Called t() on %s" % obj)
+
 
 class S1(Moos):
     pass
+
 
 class S2(Moos):
     a = None
     b = None
     m = None
     t = None
-
