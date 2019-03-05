09ed99a3f9c31a4ea04189f4c465a2112851ff43
Tonis Piip <Tonis@eggo.org>
2019-03-05 20:53:44 +0200

    insert contact test

--- tests/cypress/integration/min1/test_spec.js
+++ tests/cypress/integration/min1/test_spec.js
@@ -1,20 +1,27 @@
 describe('My First Test', function () {
-    it('can log in', function () {
+    beforeEach(() => {
+
+        // Logging in
+
         cy.visit('/');
         cy.get('ul > :nth-child(1) > a').click().log("Sign in as robin"); // sign
         cy.get('#ext-comp-1018 > .x-btn-small > :nth-child(2) > .x-btn-mc').click().log('Submit log-in');
-        cy.wait(1000);
+        cy.wait(500);
         // cy.get('#ext-gen26').click();
         // cy.type("robin");
+
+    });
+    it('Can Add a new person', function () {
         cy.get('#ext-gen20').click(); //Contacts
         cy.get('#ext-gen37').click(); //People
 
         cy.get('.x-tbar-add').click({force: true}).log("Insert");
 
-        cy.get('div:not(.x-body-masked) input[name="first_name"]').type("FooFoo");
-        cy.get('input[name="last_name"]').type("BarBar");
-        cy.get('input[name="email"]').type("FooBar@Foobar.com");
-        cy.get('input[name="gender"]+img').click();
-
+        cy.get('.l-InsertFormPanel input[name="first_name"]').type("FooFoo");
+        cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar");
+        cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com");
+        cy.get('.l-InsertFormPanel input[name="gender"]+img').click();// open
+        cy.get(".x-combo-list-item:nth-child(2)").click(); // male
+        cy.get(".l-InsertFormPanel button").click() //submit
     })
 });
