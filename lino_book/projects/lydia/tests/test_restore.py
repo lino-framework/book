ce3b2ffcb5b6846c4b201282c7f541f0ca2c3b3c
Luc Saffre <luc.saffre@gmail.com>
2019-05-24 08:17:19 +0300

    http://luc.lino-framework.org/blog/2019/0524.html

--- lino_book/projects/lydia/tests/test_restore.py
+++ lino_book/projects/lydia/tests/test_restore.py
@@ -9,5 +9,9 @@ Test whether dump the previous versions can be restored
 from lino.utils.djangotest import RestoreTestCase
 
 class TestCase(RestoreTestCase):
-    tested_versions = ['18.12.0']
+    # tested_versions = ['18.12.0']
+    tested_versions = []
+
+    # as long as there is only one production site, testing the migrators
+    # would cause increased maintenance effort.
 
