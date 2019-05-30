fbdd3082d0dd6c215ae8bdc211e17cd0fa143038
Luc Saffre <luc.saffre@gmail.com>
2019-05-30 06:35:50 +0300

    http://luc.lino-framework.org/blog/2019/0530.html

--- lino_book/projects/min9/myscript.py
+++ lino_book/projects/min9/myscript.py
@@ -10,6 +10,6 @@ where NNN is the id of some existing partner.
 import sys
 from lino.api.shell import *
 if len(sys.argv) != 2:
-    print __doc__
+    print(__doc__)
     sys.exit(-1)
-print contacts.Partner.objects.get(pk=sys.argv[1])
+print(contacts.Partner.objects.get(pk=sys.argv[1]))
