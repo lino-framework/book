0a0d0614452307fc2347eb820de93dbc3383ae4c
Luc Saffre <luc.saffre@gmail.com>
2019-07-01 03:31:30 +0300

    http://luc.lino-framework.org/blog/2019/0701.html

--- lino_book/projects/roger/tests/test_faggio.py
+++ lino_book/projects/roger/tests/test_faggio.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013-2017 Rumma & Ko Ltd
-# This file is part of Lino Voga.
-#
-# Lino Voga is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Lino Voga is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with Lino Voga.  If not, see
-# <http://www.gnu.org/licenses/>.
-
+# License: BSD (see file COPYING for details)
 """Tests about generating automatic events of a course.  Look at the
 source code!
 
