9294bb0f69ee9851f0451891f3837d9f85c00584
Luc Saffre <luc.saffre@gmail.com>
2020-01-03 08:07:12 +0200

    http://luc.lino-framework.org/blog/2020/0103.html

--- lino_book/setup_info.py
+++ lino_book/setup_info.py
@@ -1,10 +1,8 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2009-2019 Rumma & Ko Ltd
+# Copyright 2009-2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 
 # python setup.py test -s tests.test_misc.PackagesTests
-
-from __future__ import unicode_literals
 
 import sys
 
@@ -41,7 +39,7 @@ install_requires = [
 
 SETUP_INFO = dict(
     name='lino_book',
-    version='19.12.0',
+    version='20.1.0',
     install_requires=install_requires,
     # dependency_links=[
     #     'git+https://github.com/cylonoven/django-mailbox.git#egg=django_mailbox'],
