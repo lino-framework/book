2d1767ebce6275b2f260474d9bad386af9a9686e
tonis <tonis.piip@gmail.com>
2020-06-17 14:37:11 +0300

    Fix e2e tests and replace cgi.escape with html

--- tests/cypress/integration/avanti1/test_confirmation_callback.js
+++ tests/cypress/integration/avanti1/test_confirmation_callback.js
@@ -13,34 +13,37 @@ describe('Connecting ...', function () {
     });
     it('Create First client', function () {
         cy.server();
+        cy.route('/api/**').as('getData');
+
         cy.get('.l-mainmenu button:first').click(); //Contacts
-        cy.get("a[class='x-menu-item']").eq(2).click().wait(20); //People
-        cy.get("button[class=' x-btn-text x-tbar-add']").click({force: true}).log("Insert").wait(20);
-        cy.get('.l-InsertFormPanel input[name="first_name"]').type("FooFoo");
-        cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar");
-        cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com");
-        cy.get('.l-InsertFormPanel input[name="gender"]+img').click(); open
+        cy.get("a[class='x-menu-item']").eq(2).click().wait("@getData"); //People
+        cy.get("button[class=' x-btn-text x-tbar-add']").click({force: true}).log("Insert").wait("@getData");
+        cy.get('.l-InsertFormPanel input[name="first_name"]').type("A").wait(20);
+        cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar").wait(20);
+        cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com").wait(20);
+        cy.get('.l-InsertFormPanel input[name="gender"]+img').click();
         cy.get(".x-combo-list-item:nth-child(2)").click();  //male
-        cy.get(".l-InsertFormPanel button").click().wait(1000); //submit
+        cy.get(".l-InsertFormPanel button").click().wait("@getData"); //submit
+        cy.get(".l-DetailFormPanel").log("Confirm creation of person by geting detail window")
+        cy.get(".l-DetailFormPanel .x-panel.x-box-item").contains("A")
         //cy.get('div:contains("Yes")').click().wait(1000); //submit
-        if (cy.get('body').find(".l-confirmation").length) {
-            cy.get(".l-confirmation button:nth(1)").click({force:true});
-        }
+        // if (cy.get('body').find(".l-confirmation").length) {
+        //     cy.get(".l-confirmation button:nth(1)").click({force:true});
+        // }
     });
     it('Create second client with same name', function () {
         cy.server();
         cy.get('.l-mainmenu button:first').click(); //Contacts
-        cy.get("a[class='x-menu-item']").eq(2).click(); //People
-        cy.get("button[class=' x-btn-text x-tbar-add']").click({force: true}).log("Insert").wait(20);
-        cy.get('.l-InsertFormPanel input[name="first_name"]').type("FooFoo");
-        cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar");
-        cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com");
-        cy.get('.l-InsertFormPanel input[name="gender"]+img').click(); open
+        cy.get("a[class='x-menu-item']").eq(2).click().wait(20); //People
+        cy.get("button[class=' x-btn-text x-tbar-add']").click({force: true}).log("Insert").wait(100);
+        cy.get('.l-InsertFormPanel input[name="first_name"]').type("A").wait(20);
+        cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar").wait(20);
+        cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com").wait(20);
+        cy.get('.l-InsertFormPanel input[name="gender"]+img').click();
         cy.get(".x-combo-list-item:nth-child(2)").click();  //male
         cy.get(".l-InsertFormPanel button").click().wait(1000); //submit
-        if (cy.get('body').find(".l-confirmation").length) {
-            cy.get(".l-confirmation button:nth(1)").click({force:true});
-        }
+        cy.get(".l-confirmation button:nth(1)").click();
+
 
     });
 
@@ -53,21 +56,26 @@ describe('Connecting ...', function () {
 
         // cy.get('.x-tbar-add').click({force: true}).log("Insert").wait(20);
         //
-        // cy.get('.l-InsertFormPanel input[name="first_name"]').type("FooFoo");
+        // cy.get('.l-InsertFormPanel input[name="first_name"]').type("A");
         // cy.get('.l-InsertFormPanel input[name="last_name"]').type("BarBar");
         // cy.get('.l-InsertFormPanel input[name="email"]').type("FooBar@Foobar.com");
         // cy.get('.l-InsertFormPanel input[name="gender"]+img').click();// open
         // cy.get(".x-combo-list-item:nth-child(2)").click(); // male
         // cy.get(".l-InsertFormPanel button").click().wait(1000); //submit
-        cy.get(".x-toolbar-cell input[type='text']:first").type("FooFoo").wait(1000); //submit
+        cy.get(".x-toolbar-cell input[type='text']:first").type("BarBar").wait(1000); //submit
 
         // const text_initial_record_count = cy.get("div[class='xtb-text']").text();
         // const initial_record_count = parseInt(text_initial_record_count.split(" ")[text_initial_record_count.length - 1]);
         cy.wait(100).get("div[class='x-grid3-row    x-grid3-row-first']:first").click();
         cy.wait(100).get("div[class='x-grid3-row    x-grid3-row-first']:first").get("td[tabindex='0']:first").type("{del}").log("Selecting a record ...");
         // cy.wait(100).get("x-grid3-scroller")
-        cy.get(".l-confirmation button:nth(1)", ).click({force:true}); // nth(0) created a bad callback which erros, use 1 for yes 3 for no*
+        cy.get(".l-confirmation button:nth(1)", ).click({force:true}).wait(3000); // nth(0) created a bad callback which erros, use 1 for yes 3 for no*
 
+        // delete second row
+        cy.wait(100).get("div[class='x-grid3-row    x-grid3-row-first']:first").click();
+        cy.wait(100).get("div[class='x-grid3-row    x-grid3-row-first']:first").get("td[tabindex='0']:first").type("{del}").log("Selecting a record ...");
+        // cy.wait(100).get("x-grid3-scroller")
+        cy.get(".l-confirmation button:nth(1)", ).click({force:true}); // nth(0) created a bad callback which erros, use 1 for yes 3 for no*
         // const text_new_record_count = cy.get("div[class='xtb-text']").text();
         // const new_record_count = parseInt(text_new_record_count.split(" ")[text_new_record_count.length - 1]);
         // expect(initial_record_count - new_record_count ).to.include(1);
